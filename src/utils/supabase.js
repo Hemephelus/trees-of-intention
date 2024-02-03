ca2e4297a7415ebc61b970c9f046d1b76aae18e3
uzoma <chidumaga@gmail.com>
2024-02-03 12:34:25 +0100

    adds environment variable

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://feqosmhgbbpofepfjrol.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlcW9zbWhnYmJwb2ZlcGZqcm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ3Njg5MzUsImV4cCI6MjAwMDM0NDkzNX0.gW0lvsUXEv9ztvhfddnqgyJVihs0Wc7E4o6ix6rUbWM'
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 // const clientB = createClient(SUPABASE_URL, SUPABASE_KEY)
-
-
